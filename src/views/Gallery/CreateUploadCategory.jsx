f92f2fc5070301c09d279507c54ef00400d72723
sebo <contact@sebastiansimko.sk>
2020-12-03 07:34:54 +0100

    add error gallery create

--- src/views/Gallery/CreateUploadCategory.jsx
+++ src/views/Gallery/CreateUploadCategory.jsx
@@ -1,17 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SET_PAGE_TITLE, useAppContext} from "../../context/app";
 import {useHistory} from "react-router-dom";
 import i18n from "utils/i18n";
 import UploadCategoryForm from "../../components/Gallery/UploadCategoryForm";
+import Error from "../../components/Error/Error";
 
 
 function CreateUploadCategory({galleryService}) {
     const {dispatch} = useAppContext();
+    const [error, setError] = useState(null);
     const history = useHistory();
 
     const handleCreate = values => {
         galleryService.create({...values}).then(() => {
             history.push(`/gallery`)
+        }, () => {
+          setError('errors.unknown')
         })
     };
 
@@ -22,7 +26,7 @@ function CreateUploadCategory({galleryService}) {
     return (
       <div className="container mx-auto py-4">
         <span className="form-title">{i18n.t("gallery.link.create")}</span>
-
+        <Error error={error}/>
         <UploadCategoryForm onSubmit={handleCreate}/>
       </div>
     )
