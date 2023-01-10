4534bdc6f89324a0d2cf508957858c11ae73ef1f
ssimk0 <44171355+ssimk0@users.noreply.github.com>
2023-01-10 17:41:36 +0000

    Deploying to gh-pages from  @ 5bcce231d67bf8c410f617a52862943cc8b03919 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.338f1e2dfa7c2f7161c7f857097b6420.js"
+  "/precache-manifest.6e01e2355a1e2f8117cf9db81d44b5a2.js"
 );
 
 self.addEventListener('message', (event) => {
