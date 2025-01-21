9f0937a1a1cb8f58413e8e366e1970082c4ab2d9
Sebastián Simko <contact@sebastiansimko.sk>
2025-01-21 20:50:19 +0100

    update article home access path

--- src/components/Articles/ArticlesHome.jsx
+++ src/components/Articles/ArticlesHome.jsx
@@ -1,42 +1,42 @@
-import React, {useEffect, useState} from "react";
-import {Link, useLocation} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Loader from "../Loader";
 import Pagination from "../Pagination/Pagination";
 
-function ArticleHome({articleService}) {
+function ArticleHome({ articleService }) {
   const [articles, setArticles] = useState(null);
   const query = new URLSearchParams(useLocation().search);
   const page = query.get("page");
 
   useEffect(() => {
-    articleService.getArticles({p: page || 1, s: 2}).then((data) => {
+    articleService.getArticles({ p: page || 1, s: 2 }).then((data) => {
       setArticles(data);
     })
   }, [articleService, page])
   let articleList = [];
 
-  if (articles && articles.articles && articles.articles.length) {
-    articleList = articles.articles.map((article) => {
-        return (
-          <div className="article p-2" key={article.id}>
-            <div className='p-2'>
-              <h4 className="title text-2xl"><Link to={`/articles/${article.slug}`}>{article.title}</Link></h4>
-              <p className="break-words" dangerouslySetInnerHTML={{__html: article.short}}>
-              </p>
-            </div>
+  if (articles && articles.data && articles.data.length) {
+    articleList = articles.data.map((article) => {
+      return (
+        <div className="article p-2" key={article.id}>
+          <div className='p-2'>
+            <h4 className="title text-2xl"><Link to={`/articles/${article.slug}`}>{article.title}</Link></h4>
+            <p className="break-words" dangerouslySetInnerHTML={{ __html: article.short }}>
+            </p>
           </div>
-        )
-      }
+        </div>
+      )
+    }
     )
   }
 
 
-  return articles === null ? <Loader/> : (
+  return articles === null ? <Loader /> : (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
-      {articleList}
+        {articleList}
       </div>
-      <Pagination page={articles.page} total_pages={articles.total_pages}/>
+      <Pagination page={articles.page} total_pages={articles.total_pages} />
     </div>
   )
 }
